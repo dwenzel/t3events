9f8781abf21fe036e77f7b3bf89c086a35ed6e92
Dirk Wenzel <der-wenzel@gmx.de>
2013-06-06 17:57:56 +0200

    new class Tx_T3events_Hooks_ItemsProcFunc with method user_templateLayout implemented. Method sets values for flexform field settings.templateLayout from page TS.

---
+++ Classes/Hooks/ItemsProcFunc.php
@@ -0,0 +1,82 @@
+<?php
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Dirk Wenzel <wenzel@webfox01.de>, Agentur Webfox
+ *  Michael Kasten <kasten@webfox01.de>, Agentur Webfox
+ * 	method user_templateLayout has been taken from extension tx_news by 
+ *  Georg Ringer
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ *
+ *
+ * @package t3events
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ *
+ */
+class Tx_T3events_Hooks_ItemsProcFunc {
+		
+	/**
+     * Itemsproc function to extend the selection of templateLayouts in the plugin
+     *
+     * @param array &$config configuration array
+     * @param t3lib_TCEforms $parentObject parent object
+     * @return void
+     */
+    public function user_templateLayout(array &$config, t3lib_TCEforms $parentObject) {
+            // Check if the layouts are extended by ext_tables
+        if (isset($GLOBALS['TYPO3_CONF_VARS']['EXT']['t3events']['templateLayouts'])
+                && is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['t3events']['templateLayouts'])) {
+
+                // Add every item
+            foreach ($GLOBALS['TYPO3_CONF_VARS']['EXT']['t3events']['templateLayouts'] as $layouts) {
+                $additionalLayout = array(
+                    $GLOBALS['LANG']->sL($layouts[0], TRUE),
+                    $layouts[1]
+                );
+                array_push($config['items'], $additionalLayout);
+            }
+        }
+
+            // Add tsconfig values
+        if (is_numeric($config['row']['pid'])) {
+            $pagesTsConfig = t3lib_BEfunc::getPagesTSconfig($config['row']['pid']);
+            if (isset($pagesTsConfig['tx_t3events.']['templateLayouts.']) && is_array($pagesTsConfig['tx_t3events.']['templateLayouts.'])) {
+
+                    // Add every item
+                foreach ($pagesTsConfig['tx_t3events.']['templateLayouts.'] as $key => $label) {
+                    $additionalLayout = array(
+                        $GLOBALS['LANG']->sL($label, TRUE),
+                        $key
+                    );
+                    array_push($config['items'], $additionalLayout);
+                }
+            }
+        }
+
+    }
+		
+	
+}
+
+?>
