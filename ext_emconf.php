bb04b551b004969bc764a3c10f9dbcafd00b16d7
Dirk Wenzel <der-wenzel@gmx.de>
2014-08-07 14:13:47 +0200

    dependencies adapted

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,8 +34,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.6.0-6.1.99',
-			'static_info_tables' => '2.3.2-6.0.99',
+			'typo3' => '6.2.0-6.2.99',
+			'static_info_tables' => '6.1.0-6.1.99',
 		),
 		'conflicts' => array(
 		),
