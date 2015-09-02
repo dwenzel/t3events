3020628af07bcd1e10ad2afa583b8dc8d84a2c41
Dirk Wenzel <wenzel@cps-it.de>
2015-09-02 18:31:36 +0200

    [TASK] set compatible version to  TYPO3 7.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = array (
 	array (
 		'depends' => 
 		array (
-			'typo3' => '6.2.0-7.1.99',
+			'typo3' => '6.2.0-7.4.99',
 			'static_info_tables' => '6.1.0-6.1.99',
 		),
 		'conflicts' => 
