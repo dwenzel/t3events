1a11a29d39c93c08ed9122767cdfe5eaf112380c
Dirk Wenzel <der-wenzel@gmx.de>
2015-09-05 22:17:50 +0200

    [TASK] loading indicator added. Click event for ajax navigation registered for dynamically generated items.

--- Resources/Public/JavaScript/calendar.js
+++ Resources/Public/JavaScript/calendar.js
@@ -6,22 +6,21 @@
 var t3events = {};
 
 t3events.ajax = function(url, parentObject){
+	jQuery('#loader').addClass('loading');
 	jQuery.ajax({
 			url: url,
 			context: parentObject
 		})
 		.done(function(data){
 			this.html(data);
+			jQuery('#loader').removeClass('loading');
 		})
 };
-jQuery(document).ready(function($) {
-
-	jQuery('.tx-t3events .navigation a').click(function(e) {
-		e.preventDefault();
-		element = jQuery(this);
-		calendarId = element.data('calendarid');
-		parentObject = jQuery('#' + calendarId);
-		t3events.ajax(element.attr('href'), parentObject);
-	});
-
+jQuery('.tx-t3events ').on('click', '.navigation a', function(e) {
+	e.preventDefault();
+	element = jQuery(this);
+	calendarId = element.data('calendarid');
+	parentObject = jQuery('#' + calendarId);
+	t3events.ajax(element.attr('href'), parentObject);
 });
+
