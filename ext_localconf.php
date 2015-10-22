aa58067ecbdc3f0b3bff45a5d1ee6cf5104defe4
Dirk Wenzel <wenzel@cps-it.de>
2015-10-22 23:45:39 +0200

    [TASK] ProxyClass generator implemented (adapted from tx_news by Georg Ringer)

--- ext_localconf.php
+++ ext_localconf.php
@@ -23,3 +23,17 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_befunc.php']['getFl
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Webfox\\T3events\\Command\\TaskCommandController';
 
+// Register cache frontend for proxy class generation
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['t3events'] = array(
+	'frontend' =>  \TYPO3\CMS\Core\Cache\Frontend\PhpFrontend::class,
+	'backend' => \TYPO3\CMS\Core\Cache\Backend\FileBackend::class,
+	'groups' => array(
+		'all',
+		'system',
+	),
+	'options' => array(
+		'defaultLifetime' => 0,
+	),
+);
+
+\Webfox\T3events\Utility\ClassLoader::registerAutoloader();
