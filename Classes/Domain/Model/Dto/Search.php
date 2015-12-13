7b9c4bdbe26903e55ae81317e0e5922565c0478b
Dirk Wenzel <wenzel@cps-it.de>
2015-12-14 00:53:12 +0100

    [TASK] location aware interface and trait for search implemented. Search refactored.

--- Classes/Domain/Model/Dto/Search.php
+++ Classes/Domain/Model/Dto/Search.php
@@ -17,7 +17,9 @@ use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
  *
  * @package placements
  */
-class Search extends AbstractEntity {
+class Search extends AbstractEntity
+	implements LocationAwareInterface {
+	use LocationAwareTrait;
 
 	/**
 	 * Basic search word
@@ -32,27 +34,6 @@ class Search extends AbstractEntity {
 	 * @var string
 	 */
 	protected $fields;
-
-	/**
-	 * Search location
-	 *
-	 * @var string
-	 */
-	protected $location;
-
-	/**
-	 * Search radius
-	 *
-	 * @var integer
-	 */
-	protected $radius;
-
-	/**
-	 * Bounding box
-	 *
-	 * @var \array
-	 */
-	protected $bounds;
 
 	/**
 	 * Get the subject
@@ -92,60 +73,4 @@ class Search extends AbstractEntity {
 		$this->fields = $fields;
 	}
 
-	/**
-	 * Get location
-	 *
-	 * @return string A string describing a location
-	 */
-	public function getLocation() {
-		return $this->location;
-	}
-
-	/**
-	 * Set location
-	 *
-	 * @param string $location A string describing a location
-	 * @return void
-	 */
-	public function setLocation($location) {
-		$this->location = $location;
-	}
-
-	/**
-	 * Get radius
-	 *
-	 * @return integer The search radius in meter around the search location
-	 */
-	public function getRadius() {
-		return $this->radius;
-	}
-
-	/**
-	 * Set radius
-	 *
-	 * @param integer $radius The search radius in meter
-	 * @return void
-	 */
-	public function setRadius($radius) {
-		$this->radius = $radius;
-	}
-
-	/**
-	 * Get Bounds
-	 *
-	 * @return \array An array describing a bounding box around a geolocation
-	 */
-	public function getBounds() {
-		return $this->bounds;
-	}
-
-	/**
-	 * Set Bounds
-	 *
-	 * @param \array $bounds
-	 * @return void
-	 */
-	public function setBounds($bounds) {
-		$this->bounds = $bounds;
-	}
 }
