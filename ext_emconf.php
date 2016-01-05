3a05688db6c4bc275afe37823d2d2ceabcff7e80
Dirk Wenzel <wenzel@cps-it.de>
2016-01-05 12:00:00 +0100

    [TASK] compatibility with typo3 updated

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 		array(
 			'depends' =>
 				array(
-					'typo3' => '6.2.0-7.5.99',
+					'typo3' => '6.2.0-7.99.99',
 					'php' => '5.5.0-0.0.0',
 					'static_info_tables' => '6.1.0-6.99.99',
 				),
