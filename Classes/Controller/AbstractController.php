cd4c900b1ae152835fbef876e7f7d994e2eb5746
Dirk Wenzel <wenzel@cps-it.de>
2016-02-21 01:15:00 +0100

    [BUGFIX] fixed error which prevented AbstractController from filtering by genre and event type.

--- Classes/Controller/AbstractController.php
+++ Classes/Controller/AbstractController.php
@@ -16,6 +16,8 @@ use TYPO3\CMS\Extbase\Reflection\ObjectAccess;
 use TYPO3\CMS\Extbase\Property\Exception as PropertyException;
 use Webfox\T3events\Domain\Model\Dto\DemandInterface;
 use Webfox\T3events\Domain\Model\Dto\EventDemand;
+use Webfox\T3events\Domain\Model\Dto\EventTypeAwareDemandInterface;
+use Webfox\T3events\Domain\Model\Dto\GenreAwareDemandInterface;
 use Webfox\T3events\Domain\Model\Dto\SearchAwareDemandInterface;
 use Webfox\T3events\Utility\SettingsUtility;
 
@@ -224,10 +226,16 @@ class AbstractController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControl
 						if ($demand instanceof EventDemand) {
 							$demand->setGenre($propertyValue);
 						}
+						if ($demand instanceof GenreAwareDemandInterface) {
+							$demand->setGenres($propertyValue);
+						}
 						break;
 					case 'eventTypes':
 						if ($demand instanceof EventDemand) {
 							$demand->setEventType($propertyValue);
+						}
+						if ($demand instanceof EventTypeAwareDemandInterface) {
+							$demand->setEventTypes($propertyValue);
 						}
 						break;
 					case 'sortDirection':
