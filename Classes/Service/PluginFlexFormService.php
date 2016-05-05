ab63642cf95e4548eb45f06b86976c11ee58a64b
Dirk Wenzel <wenzel@cps-it.de>
2016-05-05 16:34:40 +0200

    [TASK] PluginFlexFormService now renders SVG for period constraint legend.

--- Classes/Service/PluginFlexFormService.php
+++ Classes/Service/PluginFlexFormService.php
@@ -1,6 +1,9 @@
 <?php
 namespace Webfox\T3events\Service;
 
+use TYPO3\CMS\Core\Utility\ArrayUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 /***************************************************************
  *  Copyright notice
  *  (c) 2016 Dirk Wenzel <dirk.wenzel@cps-it.de>
@@ -18,18 +21,87 @@ namespace Webfox\T3events\Service;
  *  GNU General Public License for more details.
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
+
+/**
+ * Class PluginFlexFormService
+ *
+ * @package Webfox\T3events\Service
+ */
 class PluginFlexFormService
 {
+    const ALL_LAYERS = 'text-start,text-end,arrow-left,arrow-right,future-only,past-only,by-date,start-point,end-point,
+    future-only,left-on,left-off,right-on,right-off';
+    const HIDE_FUTURE_ONLY = 'arrow-left,text-end,end-point';
+    const HIDE_PAST_ONLY = 'arrow-right,text-start,start-point,future-only,left-off,right-off,right-on';
+
     /**
      * @param array $params
-     * @param \TYPO3\CMS\Backend\Form\FormEngine $parentObject
+     * @param \TYPO3\CMS\Backend\Form\Element\UserElement $parentObject
      * @return string
      */
     public function renderPeriodConstraintLegend($params, $parentObject)
     {
-        $respectEnableFields = $params[''];
-        $content = '<div class="foo">bar</div>';
+        $content = '';
+        if (!isset($params['row']['pi_flexform']['data'])) {
+            return $content;
+        }
+        $flexFormData = $params['row']['pi_flexform']['data'];
+        $period = ArrayUtility::getValueByPath($flexFormData, 'constraints/lDEF/settings.period/vDEF/0');
+        $respectEndDate = (bool) ArrayUtility::getValueByPath($flexFormData, 'constraints/lDEF/settings.respectEndDate/vDEF');
+
+        $content = '<div class="legend">';
+        $xmlFilePath = GeneralUtility::getFileAbsFileName('EXT:t3events/Resources/Public/Images/' . 'period_constraints.svg');
+        if (file_exists($xmlFilePath)) {
+            $svg = new \DOMDocument();
+            $svg->validateOnParse = true;
+            $svg->load($xmlFilePath);
+
+            $this->switchLayers($svg, $period, $respectEndDate);
+            $content .= $svg->saveXML();
+        }
+        $content .= '</div>';
 
         return $content;
+    }
+
+    /**
+     * Gets an array of layer ids from comma separated string
+     *
+     * @param string $layerList
+     * @return array
+     */
+    protected function getLayerIds($layerList) {
+        return GeneralUtility::trimExplode(',', $layerList);
+    }
+
+    /**
+     * @param \DOMDocument $svg
+     * @param string $period
+     * @param $respectEndDate
+     */
+    protected function switchLayers($svg, $period, $respectEndDate)
+    {
+        $hiddenLayers = [];
+        $allLayers = $this->getLayerIds(self::ALL_LAYERS);
+
+        if ($period === 'futureOnly') {
+            $hiddenLayers = $this->getLayerIds(self::HIDE_FUTURE_ONLY);
+        }
+        if ($period === 'pastOnly') {
+            $hiddenLayers = $this->getLayerIds(self::HIDE_PAST_ONLY);
+        }
+
+        $gs = $svg->getElementsByTagName('g');
+        /** @var \DOMElement $g */
+        foreach ($gs as $g) {
+            if ($g->hasAttribute('id')) {
+                if (in_array($g->getAttribute('id'), $allLayers)) {
+                    $g->setAttribute('style', 'display:inline');
+                }
+                if (in_array($g->getAttribute('id'), $hiddenLayers)) {
+                    $g->setAttribute('style', 'display:none');
+                }
+            }
+        }
     }
 }
