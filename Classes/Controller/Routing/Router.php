ba34a99fea6ff2e7b84df47856bde0167696a8f8
Dirk Wenzel <wenzel@cps-it.de>
2016-11-03 00:26:31 +0100

    [TASK] Route and Router for controller implemented

---
+++ Classes/Controller/Routing/Router.php
@@ -0,0 +1,68 @@
+<?php
+namespace Dwenzel\T3events\Controller\Routing;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ * The TYPO3 project - inspiring people to share!
+ */
+use TYPO3\CMS\Core\SingletonInterface;
+
+/**
+ * Class Router
+ * routes between controller actions
+ *
+ * @package Dwenzel\T3events\Controller\Routing
+ */
+class Router implements SingletonInterface
+{
+    /**
+     * @var array
+     */
+    protected $routes = [];
+
+    /**
+     * Adds a route
+     * If no identifier is given, the origin of route will be used
+     *
+     * @param Route $route A route
+     * @param string|null $identifier Optional identifier
+     */
+    public function addRoute($route, $identifier = null)
+    {
+        if (is_null($identifier)) {
+            $identifier = $route->getOrigin();
+        }
+
+        $this->routes[$identifier] = $route;
+    }
+
+    /**
+     * Get a route by identifier
+     *
+     * @param string $identifier Identifier
+     * @return
+     */
+    public function getRoute($identifier)
+    {
+        if (isset($this->routes[$identifier])) {
+            return $this->routes[$identifier];
+        }
+
+        //todo throw ResourceNotFoundException
+    }
+
+    /**
+     * Get all routes
+     *
+     * @return array
+     */
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
+}
---
+++ Tests/Unit/Controller/Routing/RouterTest.php
@@ -0,0 +1,118 @@
+<?php
+namespace CPSIT\T3events\Tests\Controller\Routing;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use Dwenzel\T3events\Controller\Routing\Route;
+use Dwenzel\T3events\Controller\Routing\Router;
+use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Core\Tests\UnitTestCase;
+
+/**
+ * Class RouterTest
+ *
+ * @package CPSIT\T3events\Tests\Controller\Routing
+ */
+class RouterTest extends UnitTestCase
+{
+    /**
+     * @var Router|\PHPUnit_Framework_MockObject_MockObject|\TYPO3\CMS\Core\Tests\AccessibleObjectInterface
+     */
+    protected $subject;
+
+    /**
+     * set up subject
+     */
+    public function setUp()
+    {
+        $this->subject = $this->getAccessibleMock(
+            Router::class, ['dummy']
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function classImplementsSingletonInterface()
+    {
+        $this->assertTrue(
+            $this->subject instanceof SingletonInterface
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getRoutesReturnsInitialValue()
+    {
+        $this->assertSame(
+            [],
+            $this->subject->getRoutes()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getRoutesReturnsAllRoutes()
+    {
+        $routes = ['foo' => 'bar'];
+        $this->subject->_set('routes', $routes);
+
+        $this->assertSame(
+            $routes,
+            $this->subject->getRoutes()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function addRouteAddsRouteByOrigin()
+    {
+        $origin = 'fooOriginOfRoute';
+        $mockRoute = $this->getAccessibleMock(
+            Route::class, ['getOrigin'], [$origin]
+        );
+
+        $mockRoute->expects($this->once())
+            ->method('getOrigin')
+            ->will($this->returnValue($origin));
+
+        $this->subject->addRoute($mockRoute);
+
+        $this->assertSame(
+            $mockRoute,
+            $this->subject->getRoute($origin)
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function addRouteAddsRouteByIdentifier()
+    {
+        $origin = 'fooOriginOfRoute';
+
+        $identifier = '12345';
+        $mockRoute = $this->getMock(
+            Route::class, ['getOrigin'], [$origin]
+        );
+        $mockRoute->expects($this->never())
+            ->method('getOrigin');
+        $this->subject->addRoute($mockRoute, $identifier);
+
+        $this->assertSame(
+            $mockRoute,
+            $this->subject->getRoute($identifier)
+        );
+    }
+}
