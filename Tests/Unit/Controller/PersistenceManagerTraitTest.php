1be35288a06cf68630c62c51fc85786bd191ea60
Dirk Wenzel <der-wenzel@gmx.de>
2016-11-12 18:50:28 +0100

    [TASK] PersistenceManagerTrait implemented.

---
+++ Tests/Unit/Controller/PersistenceManagerTraitTest.php
@@ -0,0 +1,60 @@
+<?php
+namespace DWenzel\T3events\Tests\Controller;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Tests\UnitTestCase;
+use DWenzel\T3events\Controller\PersistenceManagerTrait;
+use TYPO3\CMS\Extbase\Persistence\PersistenceManagerInterface;
+
+/**
+ * Class PersistenceManagerTraitTest
+ *
+ * @package DWenzel\T3events\Tests\Controller
+ */
+class PersistenceManagerTraitTest extends UnitTestCase
+{
+    /**
+     * @var PersistenceManagerTrait
+     */
+    protected $subject;
+
+    /**
+     * set up
+     */
+    public function setUp()
+    {
+        $this->subject = $this->getMockForTrait(
+            PersistenceManagerTrait::class
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function persistenceManagerCanBeInjected()
+    {
+        $persistenceManager = $this->getMockForAbstractClass(
+            PersistenceManagerInterface::class
+        );
+
+        $this->subject->injectPersistenceManager($persistenceManager);
+
+        $this->assertAttributeSame(
+            $persistenceManager,
+            'persistenceManager',
+            $this->subject
+        );
+    }
+}
