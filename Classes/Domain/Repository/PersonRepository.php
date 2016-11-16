092fefe5ff507e8286bfd2de3981c7fa7bf38f67
Dirk Wenzel <wenzel@cps-it.de>
2016-11-16 10:58:49 +0100

    [TASK] code style

--- Classes/Domain/Repository/PersonRepository.php
+++ Classes/Domain/Repository/PersonRepository.php
@@ -1,42 +1,34 @@
 <?php
 namespace DWenzel\T3events\Domain\Repository;
 
-	/***************************************************************
-	 *  Copyright notice
-	 *  (c) 2013 Dirk Wenzel <wenzel@webfox01.de>, Agentur Webfox
-	 *  Michael Kasten <kasten@webfox01.de>, Agentur Webfox
-	 *  All rights reserved
-	 *  This script is part of the TYPO3 project. The TYPO3 project is
-	 *  free software; you can redistribute it and/or modify
-	 *  it under the terms of the GNU General Public License as published by
-	 *  the Free Software Foundation; either version 3 of the License, or
-	 *  (at your option) any later version.
-	 *  The GNU General Public License can be found at
-	 *  http://www.gnu.org/copyleft/gpl.html.
-	 *  This script is distributed in the hope that it will be useful,
-	 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-	 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	 *  GNU General Public License for more details.
-	 *  This copyright notice MUST APPEAR in all copies of the script!
-	 ***************************************************************/
-use TYPO3\CMS\Extbase\Persistence\QueryInterface;
+/**
+ * This file is part of the TYPO3 CMS project.
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ * The TYPO3 project - inspiring people to share!
+ */
 use DWenzel\T3events\Domain\Model\Dto\DemandInterface;
+use TYPO3\CMS\Extbase\Persistence\QueryInterface;
 
 /**
  * @package t3events
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
  */
-
-class PersonRepository extends AbstractDemandedRepository {
-	/**
-	 * Returns an array of constraints created from a given demand object.
-	 *
-	 * @param \TYPO3\CMS\Extbase\Persistence\QueryInterface $query
-	 * @param \DWenzel\T3events\Domain\Model\Dto\DemandInterface $demand
-	 * @return array<\TYPO3\CMS\Extbase\Persistence\Generic\Qom\Constraint>
-	 */
-	public function createConstraintsFromDemand(QueryInterface $query, DemandInterface $demand) {
-		// TODO: Implement createConstraintsFromDemand() method.
-		return [];
-	}
+class PersonRepository extends AbstractDemandedRepository
+{
+    /**
+     * Returns an array of constraints created from a given demand object.
+     *
+     * @param \TYPO3\CMS\Extbase\Persistence\QueryInterface $query
+     * @param \DWenzel\T3events\Domain\Model\Dto\DemandInterface $demand
+     * @return array<\TYPO3\CMS\Extbase\Persistence\Generic\Qom\Constraint>
+     */
+    public function createConstraintsFromDemand(QueryInterface $query, DemandInterface $demand)
+    {
+        // TODO: Implement createConstraintsFromDemand() method.
+        return [];
+    }
 }
