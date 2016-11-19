660172c299b8c4110ea6b5c85d4fd809166280c3
Dirk Wenzel <der-wenzel@gmx.de>
2016-11-19 13:07:45 +0100

    [TASK] moved Event/Performances/Locations/CountViewHelper to Location/CountViewHelper. Unit tests and documentation added.

---
+++ Documentation/ViewHelpers/ViewHelpers.md
@@ -0,0 +1,16 @@
+ViewHelpers
+===========
+
+_Events_ provides some ViewHelpers for convenience.
+
+Add the namespace to your template:
+
+```
+{namespace ts=DWenzel\T3events\ViewHelpers}
+```
+
+
+## Location
+[Count](./Location/CountViewHelper.md)
+
+For more usage examples see default templates.
---
+++ Tests/Unit/ViewHelpers/Location/CountViewHelperTest.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace DWenzel\T3events\Tests\ViewHelpers\Location;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use DWenzel\T3events\Domain\Model\Event;
+use DWenzel\T3events\Domain\Model\EventLocation;
+use DWenzel\T3events\Domain\Model\Performance;
+use DWenzel\T3events\ViewHelpers\Location\CountViewHelper;
+use TYPO3\CMS\Core\Tests\UnitTestCase;
+use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
+
+/**
+ * Class CountTest
+ * @package DWenzel\T3events\Tests\ViewHelpers\Location
+ */
+class CountViewHelperTest extends UnitTestCase
+{
+    /**
+     * @var CountViewHelper|\PHPUnit_Framework_MockObject_MockObject|\TYPO3\CMS\Core\Tests\AccessibleObjectInterface
+     */
+    protected $subject;
+
+    /**
+     * set up
+     */
+    public function setUp()
+    {
+        $this->subject = $this->getAccessibleMock(
+            CountViewHelper::class, ['dummy', 'registerArgument']
+        );
+    }
+
+    /**
+     * data provider for events with different unique location count
+     */
+    public function eventDataProvider()
+    {
+        $dataSets = [];
+        $cases = [
+            // zero locations
+            [
+                'uids' => [],
+                'uniqueCount' => 0
+            ],
+            // one location
+            [
+                'uids' => [1],
+                'uniqueCount' => 1
+            ],
+            // one location
+            [
+                'uids' => [1, 1],
+                'uniqueCount' => 1
+            ],
+            // three locations
+            [
+                'uids' => [2, 3, 4],
+                'uniqueCount' => 3
+            ]
+        ];
+        foreach ($cases as $case) {
+            $locationCount = count($case['uids']);
+            $eventLocation = $this->getMock(EventLocation::class, ['getUid']);
+            $eventLocation->expects($this->exactly($locationCount))
+                ->method('getUid')
+                ->will(new \PHPUnit_Framework_MockObject_Stub_ConsecutiveCalls($case['uids']));
+            $event = $this->getMock(Event::class, ['getPerformances']);
+            $objectStorage = new ObjectStorage();
+            foreach ($case['uids'] as $uid) {
+                $performance = $this->getMock(Performance::class, ['getEventLocation']);
+                $performance->expects($this->once())
+                    ->method('getEventLocation')
+                    ->will($this->returnValue($eventLocation));
+                $objectStorage->attach($performance);
+            }
+            $event->expects($this->once())
+                ->method('getPerformances')
+                ->will($this->returnValue($objectStorage));
+            $expectedResult = $case['uniqueCount'];
+            $dataSets[] = [$event, $expectedResult];
+        }
+
+        return $dataSets;
+    }
+
+    /**
+     * @test
+     */
+    public function initializeArgumentsRegistersArgumentEvent()
+    {
+        $this->subject->expects($this->once())
+            ->method('registerArgument')
+            ->with('event', Event::class, CountViewHelper::ARGUMENT_EVENT_DESCRIPTION, true);
+        $this->subject->initializeArguments();
+    }
+
+    /**
+     * @test
+     */
+    public function renderInitiallyReturnsZero()
+    {
+        $this->assertSame(
+            0,
+            $this->subject->render()
+        );
+    }
+
+    /**
+     * @test
+     * @dataProvider eventDataProvider
+     */
+    public function renderReturnsCorrectLocationCount($event, $expectedResult)
+    {
+        $this->subject->setArguments(['event' => $event]);
+        $this->assertSame(
+            $expectedResult,
+            $this->subject->render()
+        );
+    }
+
+}
