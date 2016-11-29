72fb08f6047880cd53878af50f018f53c11960cd
Dirk Wenzel <wenzel@cps-it.de>
2016-11-29 15:34:54 +0100

    [TASK] code cleanup

--- Tests/Unit/Command/TaskCommandControllerTest.php
+++ Tests/Unit/Command/TaskCommandControllerTest.php
@@ -1,33 +1,25 @@
 <?php
 namespace DWenzel\T3events\Tests\Unit\Command;
 
-/***************************************************************
- *  Copyright notice
- *  (c) 2016 Dirk Wenzel <dirk.wenzel@cps-it.de>
- *  All rights reserved
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+/**
+ * This file is part of the TYPO3 CMS project.
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ * The TYPO3 project - inspiring people to share!
+ */
 
+use DWenzel\T3events\Command\TaskCommandController;
+use DWenzel\T3events\Domain\Factory\Dto\PerformanceDemandFactory;
 use DWenzel\T3events\Domain\Model\Dto\PerformanceDemand;
 use DWenzel\T3events\Domain\Model\Performance;
 use DWenzel\T3events\Domain\Model\PerformanceStatus;
 use DWenzel\T3events\Domain\Model\Task;
+use DWenzel\T3events\Domain\Repository\PerformanceRepository;
 use DWenzel\T3events\Domain\Repository\TaskRepository;
 use TYPO3\CMS\Core\Tests\UnitTestCase;
-use DWenzel\T3events\Command\TaskCommandController;
-use DWenzel\T3events\Domain\Factory\Dto\PerformanceDemandFactory;
-use DWenzel\T3events\Domain\Repository\PerformanceRepository;
 use TYPO3\CMS\Extbase\Persistence\Generic\PersistenceManager;
 
 /**
@@ -300,7 +292,7 @@ class TaskCommandControllerTest extends UnitTestCase
             ->with($settings)
             ->will($this->returnValue($mockDemand));
 
-        $this->performanceRepository ->expects($this->once())
+        $this->performanceRepository->expects($this->once())
             ->method('findDemanded')
             ->with($mockDemand);
 
@@ -329,7 +321,7 @@ class TaskCommandControllerTest extends UnitTestCase
         $this->performanceDemandFactory->expects($this->once())
             ->method('createFromSettings')
             ->will($this->returnValue($mockDemand));
-        $this->performanceRepository ->expects($this->once())
+        $this->performanceRepository->expects($this->once())
             ->method('findDemanded')
             ->will($this->returnValue($mockPerformanceResult));
         $mockTask->expects($this->once())
@@ -362,7 +354,7 @@ class TaskCommandControllerTest extends UnitTestCase
             ->method('createFromSettings')
             ->will($this->returnValue($mockDemand));
 
-        $this->performanceRepository ->expects($this->once())
+        $this->performanceRepository->expects($this->once())
             ->method('findDemanded')
             ->with($mockDemand);
         $this->persistenceManager->expects($this->once())
