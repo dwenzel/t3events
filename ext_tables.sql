b54035fac80cac4ac68603d341001bbd329bb222
Christian Matthes <matthes@cps-it.de>
2017-01-10 22:30:03 +0100

    [BUGFIX] Set default value for ‘tx_t3events_domain_model_person.birthday’ to ‘1000-01-01’ in ext_tables.sql
    According to MySQL-documentation (http://dev.mysql.com/doc/refman/5.7/en/datetime.html) the supported range of date type is
    '1000-01-01' to '9999-12-31'. So ‘0000-00-00’ is not a valid default value.

--- ext_tables.sql
+++ ext_tables.sql
@@ -577,7 +577,7 @@ CREATE TABLE tx_t3events_domain_model_person (
 	email varchar(255) DEFAULT '' NOT NULL,
 	www varchar(255) DEFAULT '' NOT NULL,
 	title varchar(255) DEFAULT '' NOT NULL,
-	birthday date DEFAULT '0000-00-00',
+	birthday date DEFAULT '1000-01-01',
 	tx_extbase_type varchar(255) DEFAULT '' NOT NULL,
 
 	t3ver_oid int(11) DEFAULT '0' NOT NULL,
