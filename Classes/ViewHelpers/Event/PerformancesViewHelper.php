4fd64a221dcd82efb89482951f6a1c0f5c9008f3
Dirk Wenzel <wenzel@cps-it.de>
2017-03-08 18:15:07 +0100

    [BUGFIX] missing annotation in PerformancesViewHelper fixed. Invalid order field  in flex form configuration fixed.

--- Classes/ViewHelpers/Event/PerformancesViewHelper.php
+++ Classes/ViewHelpers/Event/PerformancesViewHelper.php
@@ -24,206 +24,214 @@ use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper;
  *
  * @deprecated
  */
-class PerformancesViewHelper extends AbstractTagBasedViewHelper {
+class PerformancesViewHelper extends AbstractTagBasedViewHelper
+{
 
     /**
-	* @var \TYPO3\CMS\Extbase\Persistence\ObjectStorage
-	*/
-	protected $performances;
+     * @var \TYPO3\CMS\Extbase\Persistence\ObjectStorage<\DWenzel\T3events\Domain\Model\Performance>
+     */
+    protected $performances;
 
-	/**
-	 * @var \TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface
-	 */
-	protected $configurationManager;
+    /**
+     * @var \TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface
+     */
+    protected $configurationManager;
 
-	/**
-	 * eventRepository
-	 *
-	 * @var EventRepository
-	 */
-	protected $eventRepository;
+    /**
+     * eventRepository
+     *
+     * @var EventRepository
+     */
+    protected $eventRepository;
 
-	/**
-	 * injectEventRepository
-	 *
-	 * @param EventRepository $eventRepository
-	 * @return void
-	 */
-	public function injectEventRepository(EventRepository $eventRepository) {
-		$this->eventRepository = $eventRepository;
-	}
+    /**
+     * injectEventRepository
+     *
+     * @param EventRepository $eventRepository
+     * @return void
+     */
+    public function injectEventRepository(EventRepository $eventRepository)
+    {
+        $this->eventRepository = $eventRepository;
+    }
 
-	/**
-	 * Initialize Arguments
-	 */
-	public function initializeArguments() {
-		parent::registerArgument('event', Event::class, 'Event whose performances should be rendered.', TRUE);
-		parent::registerArgument('tagName', 'string', 'Tag name to use for enclosing container', FALSE, 'div');
-		parent::registerArgument('tagNameChildren', 'string', 'Tag name to use for child nodes', FALSE, 'span');
-		parent::registerArgument('type', 'string', 'Result type: available options are complete, list, dateRange, crucialStatus', TRUE);
-		parent::registerArgument('class', 'string', 'Class attribute for enclosing container', FALSE, 'list');
-		parent::registerArgument('classChildren', 'string', 'Class attribute for children', FALSE, 'single');
-		parent::registerArgument('classFirst', 'string', 'Class name for first child', FALSE, 'first');
-		parent::registerArgument('classLast', 'string', 'Class name for last child', FALSE, 'last');
-		parent::registerArgument('childSeparator', 'string', 'Character or string separating children entries', FALSE, ', ');
-		parent::registerArgument('dateFormat', 'string', 'A string describing the date format - see php date() for options', FALSE, 'd.m.Y');
-	}
+    /**
+     * Initialize Arguments
+     */
+    public function initializeArguments()
+    {
+        parent::registerArgument('event', Event::class, 'Event whose performances should be rendered.', TRUE);
+        parent::registerArgument('tagName', 'string', 'Tag name to use for enclosing container', FALSE, 'div');
+        parent::registerArgument('tagNameChildren', 'string', 'Tag name to use for child nodes', FALSE, 'span');
+        parent::registerArgument('type', 'string', 'Result type: available options are complete, list, dateRange, crucialStatus', TRUE);
+        parent::registerArgument('class', 'string', 'Class attribute for enclosing container', FALSE, 'list');
+        parent::registerArgument('classChildren', 'string', 'Class attribute for children', FALSE, 'single');
+        parent::registerArgument('classFirst', 'string', 'Class name for first child', FALSE, 'first');
+        parent::registerArgument('classLast', 'string', 'Class name for last child', FALSE, 'last');
+        parent::registerArgument('childSeparator', 'string', 'Character or string separating children entries', FALSE, ', ');
+        parent::registerArgument('dateFormat', 'string', 'A string describing the date format - see php date() for options', FALSE, 'd.m.Y');
+    }
 
-	/**
-	 * Render method
-	 *
-	 * @return \string
-	 */
-	public function render() {
-		$this->performances = $this->arguments['event']->getPerformances();
-		$this->tagName = $this->arguments['tagName'];
-		$this->tagNameChildren = $this->arguments['tagNameChildren'];
-		$this->classChildren = $this->arguments['classChildren'];
-		$this->class = $this->arguments['class'];
-		$this->initialize();
-		$type = $this->arguments['type'];
-		$content = '';
-		$title = '';
-		switch ($type) {
-			case 'dateRange':
-				$content = $this->getDateRange();
-				break;
-			case 'crucialStatus':
-				if ($status = $this->getCrucialStatus()) {
-					$title = $status['title'];
-					$this->class .= ' ' . $status['cssClass'];
-					if ($this->renderChildren() == NULL) {
-						$content = $status['title'];
-					}
-				}
-				break;
-			case 'lowestPrice':
-				//return raw number to allow using <f:format.currency />
-				return $this->getLowestPrice();
-				break;
-			default:
-				break;
-		}
-		$this->tag->setContent($content);
-		$this->tag->addAttribute('class', $this->class);
-		$this->tag->addAttribute('title', $title);
-		$this->tag->forceClosingTag(TRUE);
-		$this->renderChildren();
-		$content = $this->tag->render();
-		$content .= $this->renderChildren();
+    /**
+     * Render method
+     *
+     * @return \string
+     */
+    public function render()
+    {
+        $this->performances = $this->arguments['event']->getPerformances();
+        $this->tagName = $this->arguments['tagName'];
+        $this->tagNameChildren = $this->arguments['tagNameChildren'];
+        $this->classChildren = $this->arguments['classChildren'];
+        $this->class = $this->arguments['class'];
+        $this->initialize();
+        $type = $this->arguments['type'];
+        $content = '';
+        $title = '';
+        switch ($type) {
+            case 'dateRange':
+                $content = $this->getDateRange();
+                break;
+            case 'crucialStatus':
+                if ($status = $this->getCrucialStatus()) {
+                    $title = $status['title'];
+                    $this->class .= ' ' . $status['cssClass'];
+                    if ($this->renderChildren() == NULL) {
+                        $content = $status['title'];
+                    }
+                }
+                break;
+            case 'lowestPrice':
+                //return raw number to allow using <f:format.currency />
+                return $this->getLowestPrice();
+                break;
+            default:
+                break;
+        }
+        $this->tag->setContent($content);
+        $this->tag->addAttribute('class', $this->class);
+        $this->tag->addAttribute('title', $title);
+        $this->tag->forceClosingTag(TRUE);
+        $this->renderChildren();
+        $content = $this->tag->render();
+        $content .= $this->renderChildren();
 
-		return $content;
-	}
+        return $content;
+    }
 
-	/**
-	 * Get date range of performances
-	 *
-	 * @return \array
-	 */
-	public function getDateRange() {
-		$format = $this->arguments['dateFormat'];
-		if ($format === '') {
-			$format = $GLOBALS['TYPO3_CONF_VARS']['SYS']['ddmmyy'] ?: 'Y-m-d';
-		}
+    /**
+     * Get date range of performances
+     *
+     * @return \array
+     */
+    public function getDateRange()
+    {
+        $format = $this->arguments['dateFormat'];
+        if ($format === '') {
+            $format = $GLOBALS['TYPO3_CONF_VARS']['SYS']['ddmmyy'] ?: 'Y-m-d';
+        }
 
-		$timestamps = array();
-		$dateRange = '';
-		/** @var Performance $performance */
-		foreach ($this->performances as $performance) {
-			$timestamps[] = $performance->getDate()->getTimestamp();
-		}
-		sort($timestamps);
-		if (strpos($format, '%') !== FALSE) {
-			$dateRange = strftime($format, $timestamps[0]);
-			$dateRange .= ' - ' . strftime($format, end($timestamps));
-		} else {
-			$dateRange = date($format, $timestamps[0]);
-			$dateRange .= ' - ' . date($format, end($timestamps));
-		}
+        $timestamps = array();
+        $dateRange = '';
+        /** @var Performance $performance */
+        foreach ($this->performances as $performance) {
+            $timestamps[] = $performance->getDate()->getTimestamp();
+        }
+        sort($timestamps);
+        if (strpos($format, '%') !== FALSE) {
+            $dateRange = strftime($format, $timestamps[0]);
+            $dateRange .= ' - ' . strftime($format, end($timestamps));
+        } else {
+            $dateRange = date($format, $timestamps[0]);
+            $dateRange .= ' - ' . date($format, end($timestamps));
+        }
 
-		return $dateRange;
-	}
+        return $dateRange;
+    }
 
-	/**
-	 * Get crucial status over all performances. Returns the status with the highest priority.
-	 *
-	 * @return \string
-	 */
-	public function getCrucialStatus() {
-		$states = array();
-		foreach ($this->performances as $performance) {
-			$status = $performance->getStatus();
-			if ($status) {
-				array_push($states,
-					array(
-						'title' => $status->getTitle(),
-						'priority' => $status->getPriority(),
-						'cssClass' => $status->getCssClass()
-					)
-				);
-			}
-		}
-		if (count($states)) {
-			usort($states, function ($a, $b) {
-				return $a['priority'] - $b['priority'];
-			});
+    /**
+     * Get crucial status over all performances. Returns the status with the highest priority.
+     *
+     * @return \string
+     */
+    public function getCrucialStatus()
+    {
+        $states = array();
+        foreach ($this->performances as $performance) {
+            $status = $performance->getStatus();
+            if ($status) {
+                array_push($states,
+                    array(
+                        'title' => $status->getTitle(),
+                        'priority' => $status->getPriority(),
+                        'cssClass' => $status->getCssClass()
+                    )
+                );
+            }
+        }
+        if (count($states)) {
+            usort($states, function ($a, $b) {
+                return $a['priority'] - $b['priority'];
+            });
 
-			return $states[0];
-		} else {
-			return '';
-		}
-	}
+            return $states[0];
+        } else {
+            return '';
+        }
+    }
 
-	/**
-	 * Get lowest price over all performances and ticket classes.
-	 *
-	 * @return float
-	 */
-	private function getLowestPrice() {
-		$prices = array();
-		foreach ($this->performances as $performance) {
-			$ticketClasses = $performance->getTicketClass();
-			foreach ($ticketClasses as $ticketClass) {
-				$prices[] = ($ticketClass->getPrice()) ? $ticketClass->getPrice() : 0;
-			}
-		}
-		sort($prices);
+    /**
+     * Get lowest price over all performances and ticket classes.
+     *
+     * @return float
+     */
+    private function getLowestPrice()
+    {
+        $prices = array();
+        foreach ($this->performances as $performance) {
+            $ticketClasses = $performance->getTicketClass();
+            foreach ($ticketClasses as $ticketClass) {
+                $prices[] = ($ticketClass->getPrice()) ? $ticketClass->getPrice() : 0;
+            }
+        }
+        sort($prices);
 
-		return (float) $prices[0];
-	}
+        return (float)$prices[0];
+    }
 
-	/**
-	 * Injects the Configuration Manager and is initializing the framework settings
-	 *
-	 * @param \TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface $configurationManager An instance of the Configuration Manager
-	 * @return void
-	 */
-	public function injectConfigurationManager(\TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface $configurationManager) {
-		$this->configurationManager = $configurationManager;
+    /**
+     * Injects the Configuration Manager and is initializing the framework settings
+     *
+     * @param \TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface $configurationManager An instance of the Configuration Manager
+     * @return void
+     */
+    public function injectConfigurationManager(\TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface $configurationManager)
+    {
+        $this->configurationManager = $configurationManager;
 
-		$tsSettings = $this->configurationManager->getConfiguration(
-			\TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface::CONFIGURATION_TYPE_FRAMEWORK,
-			't3events',
-			't3events_events'
-		);
-		$originalSettings = $this->configurationManager->getConfiguration(
-			\TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface::CONFIGURATION_TYPE_SETTINGS
-		);
+        $tsSettings = $this->configurationManager->getConfiguration(
+            \TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface::CONFIGURATION_TYPE_FRAMEWORK,
+            't3events',
+            't3events_events'
+        );
+        $originalSettings = $this->configurationManager->getConfiguration(
+            \TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface::CONFIGURATION_TYPE_SETTINGS
+        );
 
-		// start override
-		if (isset($tsSettings['settings']['overrideFlexformSettingsIfEmpty'])) {
-			$overrideIfEmpty = \TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(',', $tsSettings['settings']['overrideFlexformSettingsIfEmpty'], TRUE);
-			foreach ($overrideIfEmpty as $key) {
-				// if flexform setting is empty and value is available in TS
-				if ((!isset($originalSettings[$key]) || empty($originalSettings[$key]))
-					&& isset($tsSettings['settings'][$key])
-				) {
-					$originalSettings[$key] = $tsSettings['settings'][$key];
-				}
-			}
-		}
+        // start override
+        if (isset($tsSettings['settings']['overrideFlexformSettingsIfEmpty'])) {
+            $overrideIfEmpty = \TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(',', $tsSettings['settings']['overrideFlexformSettingsIfEmpty'], TRUE);
+            foreach ($overrideIfEmpty as $key) {
+                // if flexform setting is empty and value is available in TS
+                if ((!isset($originalSettings[$key]) || empty($originalSettings[$key]))
+                    && isset($tsSettings['settings'][$key])
+                ) {
+                    $originalSettings[$key] = $tsSettings['settings'][$key];
+                }
+            }
+        }
 
-		$this->settings = $originalSettings;
-	}
+        $this->settings = $originalSettings;
+    }
 }
 
