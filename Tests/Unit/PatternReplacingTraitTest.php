4dcd45bdafcfad4a2be8401159ca263f38c9e2d2
Dirk Wenzel <wenzel@cps-it.de>
2017-06-23 13:38:07 +0200

    [TASK] cherrypick 4e38561b Backport of iCal view feature: We now use custom views for display of events and performances in iCal format. CallStaticTrait, PatternReplacingTrait and IcalTrait implemented.

---
+++ Tests/Unit/PatternReplacingTraitTest.php
@@ -0,0 +1,57 @@
+<?php
+namespace CPSIT\T3events\Tests\Unit;
+
+/**
+ * This file is part of the "Events" project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+use DWenzel\T3events\PatternReplacingTrait;
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+
+/**
+ * Class PatternReplacingTraitTest
+ */
+class PatternReplacingTraitTest extends UnitTestCase
+{
+    /**
+     * @var PatternReplacingTrait|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $subject;
+
+    /**
+     * set up the subject
+     */
+    public function setUp()
+    {
+        $this->subject = $this->getMockBuilder(PatternReplacingTrait::class)
+            ->setMethods(['getReplacePatterns'])
+            ->getMockForTrait();
+    }
+
+    /**
+     * @test
+     */
+    public function replacePatternReplacesCorrectly()
+    {
+        $replacePatterns = [
+            '~\R~u' => "\r\n"
+        ];
+        $initialContent = "\n";
+        $expected = "\r\n";
+        $this->subject->expects($this->once())->method('getReplacePatterns')
+            ->will($this->returnValue($replacePatterns));
+
+        $this->assertSame(
+            $expected,
+            $this->subject->replacePatterns($initialContent)
+        );
+    }
+}
---
+++ Tests/Unit/View/IcalTraitTest.php
@@ -0,0 +1,89 @@
+<?php
+namespace CPSIT\T3events\Tests\Unit\View;
+
+/**
+ * This file is part of the "Events" project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+use DWenzel\T3events\View\IcalTrait;
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+
+/**
+ * Class IcalTraitTest
+ */
+class IcalTraitTest extends UnitTestCase
+{
+    /**
+     * @var IcalTrait|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $subject;
+
+    /**
+     * set up the subject
+     */
+    public function setUp()
+    {
+        $this->subject = $this->getMockBuilder(IcalTrait::class)
+            ->setMethods(['callStatic'])
+            ->getMockForTrait();
+    }
+
+    /**
+     * @test
+     */
+    public function renderCallsParentsRenderMethod()
+    {
+        $parentContent = "foo";
+        $expected = $parentContent;
+        $this->subject->expects($this->once())->method('callStatic')
+            ->with(get_parent_class($this->subject), 'render')
+            ->will($this->returnValue($parentContent));
+
+        $this->assertSame(
+            $expected,
+            $this->subject->render()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function renderReplacesLineEndingsCorrectly()
+    {
+        $parentContent = "\n";
+        $expected = "\r\n";
+        $this->subject->expects($this->once())->method('callStatic')
+            ->with(get_parent_class($this->subject), 'render')
+            ->will($this->returnValue($parentContent));
+
+        $this->assertSame(
+            $expected,
+            $this->subject->render()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function renderTrimsContentCorrectly()
+    {
+        $parentContent = "\t\tfoo\tbar\0baz\x0B\0";
+        $expected = "foo\tbar\0baz";
+        $this->subject->expects($this->once())->method('callStatic')
+            ->with(get_parent_class($this->subject), 'render')
+            ->will($this->returnValue($parentContent));
+
+        $this->assertSame(
+            $expected,
+            $this->subject->render()
+        );
+    }
+}
