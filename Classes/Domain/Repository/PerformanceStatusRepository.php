f51221f6f31e936345e04cb4df9d7d197e133a56
Erik Rauchstein <erik.rauchstein@cps-it.de>
2017-07-31 18:55:56 +0200

    [TASK] Add PerformanceStatusRepository.

---
+++ Classes/Domain/Repository/PerformanceStatusRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace DWenzel\T3events\Domain\Repository;
+/**
+ * This file is part of the TYPO3 CMS project.
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use DWenzel\T3events\Domain\Model\Dto\DemandInterface;
+use TYPO3\CMS\Extbase\Persistence\QueryInterface;
+use TYPO3\CMS\Extbase\Persistence\Repository;
+
+class PerformanceStatusRepository extends Repository implements DemandedRepositoryInterface
+{
+    use DemandedRepositoryTrait;
+
+    /**
+     * @param QueryInterface $query
+     * @param DemandInterface $demand
+     * @return array
+     */
+    public function createConstraintsFromDemand(QueryInterface $query, DemandInterface $demand)
+    {
+        // TODO: Implement createConstraintsFromDemand() method.
+        return [];
+    }
+
+}
