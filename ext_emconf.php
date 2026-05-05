bc3cfa17419efef2209319d9505fee45bca55510
Volker Golbig <v.golbig@familie-redlich.de>
2026-05-05 15:04:29 +0200

    [UPGRADE] v13: update composer.json and ext_emconf.php for TYPO3 v13
    - PHP: ^8.1 → ^8.2
    - typo3/cms-core + typo3/cms-fluid: ^11.5 || ^12.4 → ^13.4
    - dwenzel/t3extension-tools: ^2.0 || ^4.0 → ^5.0
    - ext_emconf: version 2.0.0 → 13.0.0, typo3 depends 11.5.0-11.5.99 → 13.4.0-13.99.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,9 +10,9 @@
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = ['title' => 'Events', 'description' => 'Manage events, show teasers, list and single views.', 'category' => 'plugin', 'version' => '2.0.0', 'state' => 'stable', 'clearcacheonload' => 0, 'author' => 'Dirk Wenzel, Michael Kasten', 'author_email' => 't3events@gmx.de, kasten@webfox01.de', 'author_company' => 'Agentur Webfox GmbH, Consulting Piezunka Schamoni - Information Technologies GmbH', 'constraints' =>
+$EM_CONF[$_EXTKEY] = ['title' => 'Events', 'description' => 'Manage events, show teasers, list and single views.', 'category' => 'plugin', 'version' => '13.0.0', 'state' => 'stable', 'clearcacheonload' => 0, 'author' => 'Dirk Wenzel, Michael Kasten', 'author_email' => 't3events@gmx.de, kasten@webfox01.de', 'author_company' => 'Agentur Webfox GmbH, Consulting Piezunka Schamoni - Information Technologies GmbH', 'constraints' =>
     ['depends' =>
-        ['typo3' => '11.5.00-11.9.99', 't3extension_tools' => '1.0.0-2.99.99'], 'conflicts' =>
+        ['typo3' => '13.4.0-13.99.99', 't3extension_tools' => '1.0.0-2.99.99'], 'conflicts' =>
         [], 'suggests' =>
         []]];
 
